fix bug of normalization single channel image of MNIST and FashionMNIST

cleverhans/dataset.py
@@ -389,17 +389,18 @@ def data_mnist(datadir=tempfile.gettempdir(), train_start=0,
     assert isinstance(test_end, int)
 
     X_train = download_and_parse_mnist_file(
-        'train-images-idx3-ubyte.gz', datadir=datadir) / 255.
+        'train-images-idx3-ubyte.gz', datadir=datadir)
     Y_train = download_and_parse_mnist_file(
         'train-labels-idx1-ubyte.gz', datadir=datadir)
     X_test = download_and_parse_mnist_file(
-        't10k-images-idx3-ubyte.gz', datadir=datadir) / 255.
+        't10k-images-idx3-ubyte.gz', datadir=datadir)
     Y_test = download_and_parse_mnist_file(
         't10k-labels-idx1-ubyte.gz', datadir=datadir)
 
     X_train = np.expand_dims(X_train, -1)
     X_test = np.expand_dims(X_test, -1)
-
+    X_train = convert_image(X_train)
+    X_test = convert_image(X_test)
     X_train = X_train[train_start:train_end]
     Y_train = Y_train[train_start:train_end]
     X_test = X_test[test_start:test_end]
@@ -514,8 +515,12 @@ def transform_image(preprocessor, np_image):
 def convert_image(np_image):
     np_image = np_image.astype(np.float32)
     np_image = np.divide(np_image, 255.0)
-    mean = np.array([0.485, 0.456, 0.406])[None][None][None]
-    std = np.array([0.229, 0.224, 0.225])[None][None][None]
+    if np_image.shape[-1] == 1:
+        mean = np.array([0.456])[None][None][None]  # 单通道的时候用中间这个数字作为均值
+        std = np.array([0.224])[None][None][None]
+    elif np_image.shape[-1] == 3:
+        mean = np.array([0.485, 0.456, 0.406])[None][None][None]
+        std = np.array([0.229, 0.224, 0.225])[None][None][None]
     np_image = (np_image - mean) / std
     return np_image
 

cleverhans/generate_adv_script/recreate_image_from_npy.py
@@ -1,40 +1,63 @@
 import numpy as np
 import os
 import cv2
-
-def recreate_image(float_image):
+import copy
+import random
+import re
+def recreate_image(x):
     """
         Recreates images from a torch Tensor, sort of reverse preprocessing
 
     Args:
-        float_image (np.ndarray): N,H,W,C  Image to recreate
+        x (np.array): C,H,W format Image to recreate
 
     returns:
-        recreated_im (numpy arr): Recreated image in array
+        recreated_im (numpy arr): H,W,C format Recreated image in array
     """
-    reverse_mean = np.array([[[[-0.485, -0.456, -0.406]]]])  # 1，1，1，3
-    reverse_std = np.array([[[[1/0.229, 1/0.224, 1/0.225]]]])
-    float_image /= reverse_std
-    float_image -= reverse_mean
-    float_image[float_image > 1] = 1
-    float_image[float_image < 0] = 0
-    recreated_im = np.round(float_image * 255)
+    reverse_mean = [-0.485, -0.456, -0.406]
+    reverse_std = [1/0.229, 1/0.224, 1/0.225]
+    in_channel = x.shape[-1]
+    recreated_im = copy.copy(x)  # C, H, W
+    if in_channel == 3:
+        for c in range(in_channel):
+            recreated_im[:, :, c] /= reverse_std[c]
+            recreated_im[:, :, c] -= reverse_mean[c]
+    elif in_channel == 1:
+        recreated_im[:, :, 0] /= reverse_std[1]
+        recreated_im[:, :, 0] -= reverse_mean[1]
+    recreated_im[recreated_im > 1] = 1
+    recreated_im[recreated_im < 0] = 0
+    recreated_im = np.round(recreated_im * 255)
 
-    recreated_im = np.uint8(recreated_im)  # N, H, W, C
+    recreated_im = np.uint8(recreated_im)  # H, W, C
     return recreated_im
 
-def walk_recreate(folder, output_dir):
 
-    for sub_folder in os.listdir(folder):
 
-        npy_file = folder + '/' + sub_folder + '/' + "train.npy"
-        array = np.load(npy_file)
-        orig_images = recreate_image(array)
-        target_folder = output_dir + "/" + sub_folder
-        os.makedirs(target_folder, exist_ok=True)
-        for idx, orig_image in enumerate(orig_images):
-            cv2.imwrite(target_folder + "/{}.png".format(idx), orig_image)
-        print("write {} done".format(sub_folder))
+def recreate_npy(npy_path, output_dir):
+    adv_images = np.load(npy_path)["adv_images"]
+    select_idx = random.sample(list(np.arange(adv_images.shape[0])), 10)
+    adv_images = adv_images[select_idx]
+    for idx, adv_image in enumerate(adv_images):
+        adv_image = recreate_image(adv_image)
+        target_path = output_dir + "/{}.png".format(idx)
+        cv2.imwrite(target_path, adv_image)
+        print("write {} done".format(target_path))
 
 if __name__ == "__main__":
-    walk_recreate("/home1/machen/dataset/CIFAR-10/split_data/train", "/home1/machen/dataset/CIFAR-10/output/images")
+    extract_noise_type_pattern = re.compile("(.*?)_untargeted_train.npz")
+    for dataset in ["MNIST", "F-MNIST",]:
+        ROOT_DIR_PATH = "/home1/machen/dataset/{}/adversarial_images/".format(dataset)
+
+        for arch in os.listdir(ROOT_DIR_PATH):
+            for npy_path in os.listdir(ROOT_DIR_PATH + "/" +arch):
+                if npy_path.endswith("npz") and npy_path.endswith("train.npz"):
+                    ma = extract_noise_type_pattern.match(npy_path)
+                    noise_type = ma.group(1)
+                    npy_path = ROOT_DIR_PATH + "/" + arch + "/" + npy_path
+                    target_folder = npy_path.replace("adversarial_images", "adv_images_png")
+                    target_folder = os.path.dirname(target_folder) + "/" + noise_type
+                    os.makedirs(target_folder, exist_ok=True)
+                    if dataset in ["MNIST", "F-MNIST"]:
+                        npy_path = npy_path.replace("adversarial_images","processed_adv_img")
+                    recreate_npy(npy_path, target_folder)

cleverhans/generate_adv_script/split_data_script.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("D:/work/adversarial_example")
+sys.path.append("/home1/machen/adversarial_example")
 from cleverhans.generate_adv_script.config import CIFAR10_OUTPUT_DATA_DIR,CIFAR100_OUTPUT_DATA_DIR,\
                         MNIST_OUTPUT_DATA_DIR, FMNIST_OUTPUT_DATA_DIR, \
                         META_ATTACKER_INDEX,META_ATTACKER_PART_I,META_ATTACKER_PART_II, ROOT_DATA_DIR, SVHN_OUTPUT_DATA_DIR
@@ -8,7 +8,8 @@ import numpy as np
 from collections import defaultdict
 import random
 import argparse
-
+import glob
+import re
 def split_train(npz_folder, output_root_dir):
     for npz_path in os.listdir(npz_folder):
         if npz_path.endswith(".npz") and "train" in npz_path:
@@ -49,14 +50,6 @@ def split_train_PART_attack_type(npz_folder, output_root_dir):
     for npz_path in os.listdir(npz_folder):
         if npz_path.endswith(".npz") and "train" in npz_path:
             attack_name = npz_path[:npz_path.index("_untargeted")]
-            split_type = 1
-            if attack_name in META_ATTACKER_PART_I and attack_name not in META_ATTACKER_PART_II:
-                split_type = 1
-            elif attack_name not in META_ATTACKER_PART_I and attack_name in META_ATTACKER_PART_II:
-                split_type = 2
-            elif attack_name in META_ATTACKER_PART_I and attack_name in META_ATTACKER_PART_II:
-                split_type = 3
-
             attack_index = META_ATTACKER_INDEX.index(attack_name) + 1   # clean: 1
             npz_path = npz_folder + "/" + npz_path
             data = np.load(npz_path)
@@ -72,10 +65,22 @@ def split_train_PART_attack_type(npz_folder, output_root_dir):
             adv_images = adv_images[indexes]
             gt_label = gt_label[indexes]
             adv_image_dict = defaultdict(list)
-            for idx, label in enumerate(gt_label):
+            for idx, label in enumerate(gt_label):  # FIXME 修改为adv_pred
                 adv_image_dict[label].append(adv_images[idx])    # key = gt_label, value = image list
+
+
             for label, adv_images_list in adv_image_dict.items():
                 out_dir_names = []
+                split_type = 1
+                if label in np.arange(5).tolist():
+                    if attack_name in META_ATTACKER_PART_I and attack_name not in META_ATTACKER_PART_II:
+                        split_type = 1
+                    elif attack_name not in META_ATTACKER_PART_I and attack_name in META_ATTACKER_PART_II and label in np.arange(5,10).tolist():
+                        split_type = 2
+                    elif attack_name in META_ATTACKER_PART_I and attack_name in META_ATTACKER_PART_II:
+                        split_type = 3
+
+
                 if split_type == 1:
                     out_dir_names = ["{}/I/{}_{}".format(output_root_dir, label, attack_index)]
                 elif split_type == 2:
@@ -186,26 +191,115 @@ def split_test_PART_attack_type(npz_folder, output_root_dir):
 
                         print("save {} image files into {}".format(len(query_images),out_file_path))
 
+
+def save_npz_file(npz_path, leave_one_attack_name, attack_index, output_root_dir, train_test_str):
+    data = np.load(npz_path)
+    adv_images = data["adv_images"]
+    adv_pred = data["adv_pred"]
+    gt_label = data["gt_label"]
+    sucess_rate = data["attack_success_rate"]
+    print(sucess_rate)
+    if attack_index == 1:  # clean data
+        indexes = np.arange(adv_images.shape[0])
+    else:
+        indexes = np.where(adv_pred != gt_label)[0]
+    adv_images = adv_images[indexes]
+    gt_label = gt_label[indexes]
+    adv_image_dict = defaultdict(list)
+    for idx, label in enumerate(gt_label):
+        adv_image_dict[label].append(adv_images[idx])  # key = gt_label, value = image list
+
+    for label, adv_images_list in adv_image_dict.items():
+        if train_test_str == "train":
+            out_dir_name = "{}/{}/{}/{}_{}".format(output_root_dir, leave_one_attack_name, train_test_str,
+                                                   label, attack_index)
+            os.makedirs(out_dir_name, exist_ok=True)
+            out_file_path = "{}/{}.npy".format(out_dir_name, "train")
+            count_file_path = "{}/count.txt".format(out_dir_name)
+            adv_images = np.stack(adv_images_list)
+            fp = np.memmap(out_file_path, dtype='float32', mode='w+', shape=adv_images.shape)
+            fp[:, :, :, :] = adv_images[:, :, :, :]
+            del fp
+            with open(count_file_path, "w") as file_obj:
+                file_obj.write(str(len(adv_images)))
+                file_obj.flush()
+            print("save {} image files into {}".format(len(adv_images), out_file_path))
+        else:
+            out_dir_name = "{}/{}/{}/{}_{}".format(output_root_dir, leave_one_attack_name, train_test_str,
+                                                   label, attack_index)
+            all_index = np.arange(len(adv_images_list))
+            sub_lists = list(chunk(all_index, 2))
+            support_indexes = sub_lists[0]
+            query_indexes = sub_lists[1]
+            if len(support_indexes) >= 30:
+                support_indexes = random.sample(support_indexes, 30)
+            if len(query_indexes) >= 60:
+                query_indexes = random.sample(query_indexes, 60)
+            support_images = adv_images[support_indexes]
+            query_images = adv_images[query_indexes]
+
+            for post_str in ["support", "query"]:
+                if post_str=='support':
+                    current_imgs = support_images
+                else:
+                    current_imgs = query_images
+                dir_name = "{}/{}".format(out_dir_name, post_str)
+                os.makedirs(dir_name,exist_ok=True)
+                out_file_path = dir_name +'/' + post_str +".npy"
+                fp = np.memmap(out_file_path, dtype='float32', mode='w+', shape=current_imgs.shape)
+                fp[:, :, :, :] = current_imgs[:, :, :, :]
+                del fp
+                count_file_path = "{}/count.txt".format(dir_name)
+                with open(count_file_path, "w") as file_obj:
+                    file_obj.write(str(len(current_imgs)))
+                    file_obj.flush()
+                print("save {} image files into {}".format(len(current_imgs), out_file_path))
+
+def split_leave_one_out_attack_type(npz_folder, output_root_dir):
+
+    total_train_npz_path_list = glob.glob("{}/*train.npz".format(npz_folder))
+    extract_attack_name_pattern = re.compile(".*/(.*?)_untargeted.*")
+    for test_npz_path in glob.glob("{}/*test.npz".format(npz_folder)):  # leave one out protocol
+        ma = extract_attack_name_pattern.match(test_npz_path)
+        leave_out_attack_name = ma.group(1)
+        if leave_out_attack_name == "clean":  # clean同时存在于训练集和测试集
+            continue
+        train_npz_path_list = [path for path in total_train_npz_path_list if leave_out_attack_name not in path]
+        test_npz_path_list = [test_npz_path, "{}/clean_untargeted_test.npz".format(npz_folder)]
+        for train_npz_path in train_npz_path_list:
+            ma = extract_attack_name_pattern.match(train_npz_path)
+            current_atk_index = META_ATTACKER_INDEX.index(ma.group(1))+1
+            save_npz_file(train_npz_path, leave_out_attack_name, current_atk_index, output_root_dir, "train")
+        for test_npz_path in test_npz_path_list:
+            ma = extract_attack_name_pattern.match(test_npz_path)
+            current_atk_index = META_ATTACKER_INDEX.index(ma.group(1))+1
+            save_npz_file(test_npz_path, leave_out_attack_name,current_atk_index, output_root_dir, "test")
+
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch Meta_SGD Training')
 
-    parser.add_argument("--dataset", type=str, default="CIFAR10", choices=["CIFAR10", "CIFAR100", "MNIST", "FMNIST"],
+    parser.add_argument("--dataset", type=str, default="CIFAR-10", choices=["CIFAR-10", "SVHN", "MNIST", "F-MNIST"],
                         help="the dataset to train")
-    parser.add_argument("--adv_arch", default="shallow_4_convs",type=str, choices=["shallow_10_convs", "shallow_4_convs", "vgg16small"])
+    parser.add_argument("--adv_arch", default="conv3",type=str, choices=["conv3", "resnet10", "resnet18"])
     args = parser.parse_args()
     dataset = args.dataset
-    if dataset == "CIFAR10":
+    if dataset == "CIFAR-10":
         out_root_dir = CIFAR10_OUTPUT_DATA_DIR
-    elif dataset == "CIFAR100":
+    elif dataset == "CIFAR-100":
         out_root_dir = CIFAR100_OUTPUT_DATA_DIR
     elif dataset == "MNIST":
         out_root_dir = MNIST_OUTPUT_DATA_DIR
-    elif dataset == "FMNIST":
+    elif dataset == "F-MNIST":
         out_root_dir = FMNIST_OUTPUT_DATA_DIR
     elif dataset == "SVHN":
         out_root_dir = SVHN_OUTPUT_DATA_DIR
     out_root_dir = out_root_dir + "/{}".format(args.adv_arch)
-    split_train_PART_attack_type(out_root_dir, "{}/train".format(out_root_dir))
-    split_test_PART_attack_type(out_root_dir,  "{}/test".format(out_root_dir))
+    split_train_PART_attack_type(out_root_dir, "{}/TRAIN_I_TEST_II/train".format(out_root_dir))
+    split_test_PART_attack_type(out_root_dir, "{}/TRAIN_I_TEST_II/test".format(out_root_dir))
+
+    # split_leave_one_out_attack_type(out_root_dir, "{}/leave_one_out".format(out_root_dir))
 
 